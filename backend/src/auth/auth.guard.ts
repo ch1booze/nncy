adf7cd5bf229f60759b17bb65efba11ee9a336e4
Victor Momodu <victoramomodu@gmail.com>
2024-06-01 08:57:33 +0100

    feat: Remove express-jwt and jwks-rsa

--- backend/src/auth/auth.guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { expressJwtSecret } from 'jwks-rsa';
-
-@Injectable()
-export class AuthGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
-    return true;
-  }
-}
