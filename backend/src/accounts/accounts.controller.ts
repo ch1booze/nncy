5b9a8a0010917d5e43be242a74f202b915846962
Victor Momodu <victoramomodu@gmail.com>
2024-06-03 20:01:50 +0100

    feat: Add dummy BVN verification

--- backend/src/accounts/accounts.controller.ts
+++ backend/src/accounts/accounts.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 
 @Controller('accounts')
 export class AccountsController {
   constructor(private readonly accountsService: AccountsService) {}
+  @Post('verify-bvn')
+  async verifyBvn(@Body() bvn: string) {
+    return this.accountsService.verifyBvn(bvn);
+  }
 }
--- backend/src/accounts/accounts.service.ts
+++ backend/src/accounts/accounts.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/utils/prisma.service';
+import { ResponseDto } from 'src/utils/response.dto';
 
 @Injectable()
-export class AccountsService {}
+export class AccountsService {
+  constructor(private prismaService: PrismaService) {}
+
+  async verifyBvn(bvn: string): Promise<ResponseDto<null>> {
+    return {
+      statusCode: HttpStatus.OK,
+      message: `Verified BVN: ${bvn}.`,
+    };
+  }
+}
