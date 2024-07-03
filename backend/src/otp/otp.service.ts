c8b8b531062849940ef3838dbd27759ae8aa83cf
Victor Momodu <victoramomodu@gmail.com>
2024-07-03 01:45:29 +0100

    test: Test mail and OTP service.

--- backend/src/otp/otp.service.ts
+++ backend/src/otp/otp.service.ts
@@ -1,4 +1,4 @@
-import speakeasy from 'speakeasy';
+import * as speakeasy from 'speakeasy';
 import { ResponseDTO } from 'src/utils/response.dto';
 
 import { Injectable } from '@nestjs/common';
@@ -6,7 +6,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class OtpService {
   async generateOTP() {
-    const secret = speakeasy.generateSecret();
+    const secret = speakeasy.generateSecret({ length: 20 });
+    console.log('Something here.');
+    console.log(secret);
     const token = speakeasy.totp({ secret: secret.base32, encoding: 'base32' });
     return ResponseDTO.success('OTP has been generated.', {
       secret: secret.base32,
