52e98335a09345e2048838b1ff56ecc4542ce7d3
Victor Momodu <victoramomodu@gmail.com>
2024-07-18 17:58:45 +0100

    feat: Create agent controller.

--- backend/src/agent/agent.module.ts
+++ backend/src/agent/agent.module.ts
@@ -4,14 +4,17 @@ import { AgentFactoryService } from './agent-factory.service';
 import { AgentPromptsService } from './agent-prompts.service';
 import { AgentSystemsService } from './agent-systems.service';
 import { AgentToolsService } from './agent-tools.service';
+import { AgentController } from './agent.controller';
 import { AgentService } from './agent.service';
 
 @Module({
+  controllers: [AgentController],
+
   providers: [
     AgentFactoryService,
     AgentPromptsService,
-    AgentToolsService,
     AgentSystemsService,
+    AgentToolsService,
     AgentService,
   ],
 })
